a7e3c0f21995891c595d3d7e87f6000083936fe7
i-abdul-raheem <abdul.raheem.30140@gmail.com>
2023-03-21 00:33:58 +0500

    Add to cart feature added

--- components/cart.php
+++ components/cart.php
@@ -4,37 +4,13 @@
         <h5 id="cartLabel">Cart</h5>
         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
-    <div class="offcanvas-body">
-        <!-- Single Item -->
-        <div class="card mb-3">
-            <div class="card-body p-2 d-flex justify-content-start">
-                <img class="me-4" style="width: 75px; height: 75px; border-radius: 10px;"
-                    src="./assets/img/feature_prod_01.jpg" alt="test">
-                <div class="info p-1">
-                    <a style="text-decoration: none; color: #000;" href="#">
-                        <h4 class="card-title mb-2">Gym Weight</h4>
-                    </a>
-                    <span class="text-secondary">Price: 30.00$</span>
-                </div>
-            </div>
-            <div class="card-footer">
-                <div class="row">
-                    <div class="col">
-                        <button class="btn"><i class="fa fa-chevron-down"></i></button>
-                        <input style="width: 40px; border: none; background-color: transparent; text-align: center;"
-                            type="text" value="0" name="qty" id="qty" readonly />
-                        <button class="btn"><i class="fa fa-chevron-up"></i></button>
-                    </div>
-                    <div class="col text-end"><button class="btn btn-danger"><i class="fa fa-trash"></i></button></div>
-                </div>
-            </div>
-        </div>
-        <!-- End Single Item -->
-    </div>
+    <div class="offcanvas-body" id="my-cart"></div>
     <div class="offcanvas-footer p-3 bg-light d-flex justify-content-between">
-        <button class="btn btn-success">Chechout</button>
+        <a href="#checkout" class="btn btn-success">Chechout</a>
+        <!-- <a class="nav-icon position-relative text-decoration-none btn btn-success" data-bs-toggle="offcanvas"
+            data-bs-target="#account" aria-controls="account">Login</a> -->
         <h4 class="total">
-            Total: 30.00$
+            Total: $<span id="myTotalPrice"></span>
         </h4>
     </div>
 </div>
--- components/header.php
+++ components/header.php
@@ -35,7 +35,8 @@
                     data-bs-target="#cart" aria-controls="cart">
                     <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
                     <span
-                        class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">0</span>
+                        class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark"
+                        id="totalItems"></span>
                 </a>
                 <a class="nav-icon position-relative text-decoration-none" data-bs-toggle="offcanvas"
                     data-bs-target="#account" aria-controls="account">
