664f3358e58eac2c523c2f557281b60dd84d7ca1
Muhammed Abuzar <112900805+i-abdul-raheem@users.noreply.github.com>
2023-03-21 22:16:32 +0500

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,5 +1,5 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_NAME", "zarsaw");
-define("DB_USER", "root");
-define("DB_PASS", "toor");
+define("DB_NAME", "u914459605_zarsaw");
+define("DB_USER", "u914459605_zarsaw");
+define("DB_PASS", "hEllo@911");
