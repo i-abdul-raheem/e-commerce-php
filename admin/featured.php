118c3172583f66580ae76dc69b68d136ec88f04c
i-abdul-raheem <abdul.raheem.30140@gmail.com>
2023-04-04 04:28:28 +0500

    featured products ui update

--- admin/featured.php
+++ admin/featured.php
@@ -17,18 +17,7 @@ require("./components/header.php");
                         <th>Featured</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <th>Shoes</th>
-                        <td><img style="width: 100px;" src="../assets/img/banner_img_01.jpg" alt="Banner 1" /></td>
-                        <td>CATEGOTY</td>
-                        <td>
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="customSwitches">
-                            </div>
-                        </td>
-                    </tr>
+                <tbody id="myContent">
                 </tbody>
             </table>
         </div>
@@ -37,4 +26,35 @@ require("./components/header.php");
 
 <script>
     document.getElementById('featuredMenu').className = "active";
+
+    async function populateContent() {
+        const target = document.getElementById("myContent");
+        target.innerHTML = "";
+        const res = await fetch("../api/products.php").then(res => res.json());
+        for (let i of res.data) {
+            target.innerHTML += `
+                    <tr>
+                        <td>${i.product_id}</td>
+                        <th>${i.title}</th>
+                        <td><img style="width: 100px;" src="../assets/img/${i.image}" alt="Banner 1" /></td>
+                        <td id="cat-${i.product_id}"></td>
+                        <td>
+                            <div class="custom-control custom-switch">
+                                <input type="checkbox" class="custom-control-input" id="check-${i.product_id}" />
+                            </div>
+                        </td>
+                    </tr>
+        `;
+            getCategoryName(i.category, i.product_id);
+            if (i.featured == 1) {
+                document.getElementById(`check-${i.product_id}`).checked = true;
+            }
+        }
+    }
+
+    async function getCategoryName(id, ref) {
+        const res = await fetch(`../api/categories.php?id=${id}`).then(res => res.json());
+        document.getElementById(`cat-${ref}`).innerHTML = res?.data?.title || '-';
+    }
+    populateContent();
 </script>
